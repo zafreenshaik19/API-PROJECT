5fefa3b5ad5a92ebfdd59dcb2e68b98ba56c295a
zafreen1907 <zafreenshaik123@gmail.com>
2026-06-27 16:52:18 +0530

    Remove old files and keep Dog Image Generator

--- app1.js
+++
@@ -1,6 +0,0 @@
-async function dog() {
-  const data = await fetch("https://dog.ceo/api/breeds/image/random");
-  const dogData = await data.json();
-  const finalResult = dogData.message;
-  document.getElementById("image").innerHTML = `<img src="${finalResult}" alt="" width="320" />`;
-}
